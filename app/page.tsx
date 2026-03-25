ced2890d276dec95f068728d96eebf0b352fa693
Anubhav yadav <anubhav1374@gmail.com>
2026-03-25 23:03:41 +0530

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div className="">
     <h1>Welcome to Home</h1>
+    <Button>Helllooo</Button>
    </div>
   );
 }
