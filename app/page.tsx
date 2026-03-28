cc66b664d7fa32cbc875d04cf78c49738ca7470c
Anubhav yadav <anubhav1374@gmail.com>
2026-03-28 13:00:18 +0530

    Initial Changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,10 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div className="">
-    <h1>Welcome to Home</h1>
+    <h1 className="font-game">Welcome to Home</h1>
     <Button>Helllooo</Button>
+
+
    </div>
   );
 }
